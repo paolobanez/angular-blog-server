7a06f4c1c8fa1ea42b677b71c6e7b10dd7a049bd
paolo banez <paolo.banez@intechsive.com>
2020-02-09 21:14:12 +0800

    fetch single article

--- articles.js
+++ articles.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
   app.get("/api/articles", function(request, response) {
     response.send(ARTICLES);
   });
+
+  app.get("/api/articles/:key", function(request, response) {
+    response.send(
+      ARTICLES.filter(article => article.key === request.params.key)[0]
+    );
+  });
 };
