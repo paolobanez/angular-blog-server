8a1cf9549d2d952e425c1ff4c26666ad4b5e85da
paolo banez <paolo.banez@intechsive.com>
2020-02-09 21:09:24 +0800

    added mock articles

---
+++ mock-articles.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: 1,
+    title: "My first article",
+    content:
+      "Aliquam erat volutpat. Nulla gravida orci sit amet libero lacinia, in interdum ex vestibulum. Morbi purus nunc, tristique quis augue vitae, mattis dictum velit. Proin mattis dui aliquet turpis luctus vehicula non ut nunc. Ut fringilla, tortor id tristique euismod, urna neque tincidunt est, nec commodo nisi augue nec sapien. In sed tempus est. Suspendisse hendrerit aliquam nisl, non commodo purus egestas nec. Fusce lacinia faucibus tortor, eget faucibus risus molestie ac.",
+    description: "This is my first article! It's great. Please read it. :)",
+    key: "my-first-article",
+    date: new Date(),
+    imageUrl: "http://angular.io/assets/images/logos/angular/angular.png"
+  },
+  {
+    id: 2,
+    title: "My second article",
+    content:
+      "Nulla sed arcu eget augue viverra tempus. Mauris nec turpis et dui ornare auctor a in orci. Donec luctus sapien ac aliquet dapibus. Sed consequat orci eget consectetur maximus. Nam nibh quam, eleifend at viverra feugiat, tristique sit amet felis. Nam vitae mattis nisl. Suspendisse potenti. Integer pretium eget magna sed maximus. Vestibulum pulvinar nunc in ex porta molestie.",
+    description: "Also a great article!",
+    key: "the-second-article",
+    date: new Date(),
+    imageUrl:
+      "http://angular.io/assets/images/logos/angular/angular_solidBlack.png"
+  }
+];
--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(8000, () => {
   console.log("Server is started and listening.");
 });
+
+app.get("/api/", function(request, response) {
+  response.send("Hello Node.js!");
+});
+
+require("./articles")(app);
